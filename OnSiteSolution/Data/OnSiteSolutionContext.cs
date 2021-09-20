38b70e3dff7eb5451febd0545d1c043559454c4b
DS062 <DS062>
2021-09-20 07:47:03 +0530

    Add project files.

---
+++ OnSiteSolution/Data/OnSiteSolutionContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OnSiteSolution.Model;
+
+namespace OnSiteSolution.Data
+{
+    public class OnSiteSolutionContext : DbContext
+    {
+        public OnSiteSolutionContext (DbContextOptions<OnSiteSolutionContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<OnSiteSolution.Model.Clients> Clients { get; set; }
+
+        public DbSet<OnSiteSolution.Model.Message> Message { get; set; }
+    }
+}
---
+++ OnSiteSolution/Migrations/20210918163323_OnSite.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OnSiteSolution.Migrations
+{
+    public partial class OnSite : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // taking data from another table
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Text = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    time = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.Id);
+                });
+
+            // creating table 
+            migrationBuilder.CreateTable(
+                name: "Message",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Text = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Sender = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    time = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Message", x => x.Id);
+                });
+        }
+
+        // for frop files
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clients");
+
+            migrationBuilder.DropTable(
+                name: "Message");
+        }
+    }
+}
